326e694e72373adb1ff750d7a92f0827bc6c7eea
VEENA-21 <beriveenaprathap@gmail.com>
2024-08-02 11:16:54 +0530

    changed the normal localhost url to live API_URL

--- src/components/data.js
+++ src/components/data.js
@@ -1,5 +1,5 @@
 
 
-export const API_URL = 'http://localhost:5000'
+// export const API_URL = 'http://localhost:5000'
 
-// export const API_URL ='https://neonflake-backend-k2ln.onrender.com'
+export const API_URL ='https://neonflake-backend-k2ln.onrender.com'
